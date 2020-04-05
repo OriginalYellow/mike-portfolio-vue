6aa22580981fa8ebbc7807997abc2314f8b42690
OriginalYellow <michael.robert.joseph1@gmail.com>
2020-04-05 19:03:24 -0400

    add tap-nyan; major template refactor

---
+++ components/SectionContainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container pl-8 pr-24 md:px-64">
+    <slot />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <client-only>
+      <!-- floating links -->
       <div
         id="links"
         class="fixed top-24 right-24"
@@ -21,17 +22,24 @@
         </a>
       </div>
 
+      <!-- page body -->
       <full-page
         ref="fullpage"
         :options="options"
       >
+        <!-- intro section -->
         <div class="section">
-          <div class="container pl-8 pr-24 md:px-64">
+          <div :class="containerClass">
             <div class="md:mb-0 mb-24">
-              <h1 :class="{'h1': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'h3': ($mq === 'sm') || ($mq === 'default') }">
+              <h1 :class="largeHeadlineClass">
                 hi, <br v-if="$mq === 'sm'">i'm mike
               </h1>
-              <p :class="{'h5': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'body': ($mq === 'sm') || ($mq === 'default') }">
+              <p
+                :class="{
+                  'h5': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'),
+                  'body': ($mq === 'sm') || ($mq === 'default') }
+                "
+              >
                 And I like to build things.
               </p>
             </div>
@@ -44,78 +52,49 @@
           </div>
         </div>
 
+        <!-- summary section -->
         <div class="section">
-          <div class="container pl-8 pr-24 md:px-64">
-            <h1 :class="{'h1': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'h3': ($mq === 'sm') || ($mq === 'default') }">
+          <div :class="containerClass">
+            <h1 :class="largeHeadlineClass">
               what i'm all about
             </h1>
-            <p :class="{'body': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'body-2': ($mq === 'sm') || ($mq === 'default') }">
-              I am a design-conscious full-stack developer who specializes in webapp development. Having worked at a number of startups, my experience also includes roles where I’ve had to wear many different hats – project manager, data architect, product designer, UX designer, and visual designer among others.
+            <p :class="bodyClass">
+              {{ summaryBody }}
             </p>
           </div>
         </div>
 
+        <!-- projects section -->
         <div class="section">
           <div class="slide">
-            <div class="container pl-8 pr-24 md:px-64">
-              <h1 :class="{'h1': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'h3': ($mq === 'sm') || ($mq === 'default') }">
+            <div :class="containerClass">
+              <h1 :class="largeHeadlineClass">
                 things <br v-if="$mq === 'sm'">i've built
               </h1>
-              <p :class="{'body': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'body-2': ($mq === 'sm') || ($mq === 'default') }">
-                Since I started doing freelance and entrepreneurial work, I have become a “one-man” development and design team, with several live products under my belt so far - the marketing site for an NPO called "Beyond Compassion", and the web app for a premium dating service called "The Society".
+              <p :class="bodyClass">
+                {{ projectsSectionFirstSlideBody }}
               </p>
             </div>
           </div>
-          <div class="slide">
-            <div class="container pl-8 pr-24 md:px-64">
-              <h2 :class="{'h3': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'h5': ($mq === 'sm') || ($mq === 'default') }">
+
+          <div
+            v-for="project in projects"
+            :key="project.name"
+            class="slide"
+          >
+            <div :class="containerClass">
+              <h2 :class="smallHeadlineClass">
                 <a
-                  href="https://thesociety.app/"
+                  :href="project.link"
                   target="_blank"
                   class="underline"
-                >thesociety.app</a>
+                >{{ project.name }}</a>
               </h2>
-              <p :class="{'body': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'body-2': ($mq === 'sm') || ($mq === 'default') }">
-                I am part of a small and, other than myself, completely non-technical team working on "The Society" - a hands-on matchmaking dating service that offers an alternative to our algorithm-based competitors. I built the companion webapp mostly with vue, vuetify, apollo-server, prisma, mysql, and zeit now. The design is almost entirely my own except for a few things, like the logo and other branding, which were outsourced to a design firm.
+              <p :class="bodyClass">
+                {{ project.body }}
               </p>
             </div>
           </div>
-          <div class="slide">
-            <div class="container pl-8 pr-24 md:px-64">
-              <h2 :class="{'h3': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'h5': ($mq === 'sm') || ($mq === 'default') }">
-                <a
-                  href="https://bccharity.org/"
-                  target="_blank"
-                  class="underline"
-                >bccharity.org</a>
-              </h2>
-              <p :class="{'body': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'body-2': ($mq === 'sm') || ($mq === 'default') }">
-                I built the marketing site for a charity called "Beyond Compassion" entirely on my own, minus some art and photography assets. The site is built on vue, vuetify, nuxt, zeit now, and I used storyblok for the CMS to give my client the most user-friendly content publishing experience possible.
-              </p>
-            </div>
-          </div>
-          <div class="slide">
-            <div class="container pl-8 pr-24 md:px-64">
-              <h2 :class="{'h3': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'h5': ($mq === 'sm') || ($mq === 'default') }">
-                <a
-                  href="https://member.buzz/"
-                  target="_blank"
-                  class="underline"
-                >member.buzz</a>
-              </h2>
-              <p :class="{'body': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'body-2': ($mq === 'sm') || ($mq === 'default') }">
-                I was one of two full-stack engineers at Member.buzz, where we worked on a live web app of the same name. Member.buzz is site builder similar to squarespace but is also an organization management platform targeted at NPO's and clubs. It is built on ASP.NET MVC, entity framework, Azure, DevExpress, bootstrap, and jQuery. My major contributions were to the WYSIWYG static page builder as well the analytics dashboard.
-              </p>
-            </div>
-          </div>
-          <!-- <div class="container px-16 md:px-64">
-            <h1 :class="{'h1': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'h3': ($mq === 'sm') || ($mq === 'default') }">
-              stuff i've built
-            </h1>
-            <p :class="{'body': ($mq === 'md') || ($mq === 'lg') || ($mq === 'xl'), 'body-2': ($mq === 'sm') || ($mq === 'default') }">
-              [coming soon]
-            </p>
-          </div> -->
         </div>
       </full-page>
     </client-only>
@@ -137,6 +116,29 @@ export default {
 
   data () {
     return {
+      summaryBody: 'I am a design-conscious full-stack developer who specializes in webapp development. Having worked at a number of startups, my experience also includes roles where I’ve had to wear many different hats – project manager, data architect, product designer, UX designer, and visual designer among others.',
+      projectsSectionFirstSlideBody: 'Since I started doing freelance and entrepreneurial work, I have become a “one-man” development and design team, with several live products under my belt so far - the marketing site for an NPO called "Beyond Compassion", and the web app for a premium dating service called "The Society".',
+      projects: [
+        {
+          name: 'thesociety.app',
+          link: 'https://thesociety.app/',
+          body:
+            'I am part of a small and, other than myself, completely non-technical team working on "The Society" - a hands-on matchmaking dating service that offers an alternative to our algorithm-based competitors. I built the companion webapp mostly with vue, vuetify, apollo-server, prisma, mysql, and zeit now. The design is almost entirely my own except for a few things, like the logo and other branding, which were outsourced to a design firm.'
+        },
+        {
+          name: 'bccharity.org',
+          link: 'https://bccharity.org/',
+          body:
+            'I built the marketing site for a charity called "Beyond Compassion" entirely on my own, minus some art and photography assets. The site is built on vue, vuetify, nuxt, zeit now, and I used storyblok for the CMS to give my client the most user-friendly content publishing experience possible.'
+        },
+        {
+          name: 'member.buzz',
+          link: 'https://thesociety.app/',
+          body:
+            "I was one of two full-stack engineers at Member.buzz, where we worked on a live web app of the same name. Member.buzz is site builder similar to squarespace but is also an organization management platform targeted at NPO's and clubs. It is built on ASP.NET MVC, entity framework, Azure, DevExpress, bootstrap, and jQuery. My major contributions were to the WYSIWYG static page builder as well the analytics dashboard."
+        }
+      ],
+      containerClass: 'container pl-8 pr-24 md:px-64',
       options: {
         licenseKey: process.env.FULLPAGE_KEY,
         scrollHorizontallyKey: process.env.SCROLL_HORIZONTALLY_KEY,
@@ -156,6 +158,27 @@ export default {
   },
 
   computed: {
+    largeHeadlineClass () {
+      return {
+        h1: this.$mq === 'md' || this.$mq === 'lg' || this.$mq === 'xl',
+        h3: this.$mq === 'sm' || this.$mq === 'default'
+      }
+    },
+
+    smallHeadlineClass () {
+      return {
+        h3: this.$mq === 'md' || this.$mq === 'lg' || this.$mq === 'xl',
+        h5: this.$mq === 'sm' || this.$mq === 'default'
+      }
+    },
+
+    bodyClass () {
+      return {
+        body: this.$mq === 'md' || this.$mq === 'lg' || this.$mq === 'xl',
+        'body-2': this.$mq === 'sm' || this.$mq === 'default'
+      }
+    },
+
     scalingFactor () {
       switch (this.$mq) {
         case 'sm':
